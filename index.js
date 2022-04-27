1129dd7fd8fa1d4969a3426b7e7f29b9a2dc304b
jricardocam <jricardocam@gmail.com>
2022-04-26 20:37:54 -0500

    Reduciendo lineas de codigo en ExplorerService

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Reader = require("./lib/utils/Reader")
 const ExplorerService=require("./lib/services/ExplorerService")
+const FizzbuzzService=require("./lib/services/FizzbuzzService")
 
 const listaExplorers = Reader.readJsonFile("explorers.json")
-console.log(ExplorerService.filterByMission(listaExplorers))
-console.log(ExplorerService.getAmountOfExplorersByMission(listaExplorers))
-console.log(ExplorerService.getExplorersUsernamesByMission(listaExplorers))
+console.log(ExplorerService.filterByMission(listaExplorers,"node"))
+console.log(ExplorerService.getAmountOfExplorersByMission(listaExplorers,"java"))
+console.log(ExplorerService.getExplorersUsernamesByMission(listaExplorers,"java"))
