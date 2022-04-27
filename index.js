d723ef57c932cf7a22b03fef549900d9273da696
jricardocam <jricardocam@gmail.com>
2022-04-26 20:07:47 -0500

    Agregando ExplorerService con sus metodos

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const Reader = require("./lib/utils/Reader")
+const ExplorerService=require("./lib/services/ExplorerService")
 
 const listaExplorers = Reader.readJsonFile("explorers.json")
-console.log(listaExplorers)
+console.log(ExplorerService.filterByMission(listaExplorers))
+console.log(ExplorerService.getAmountOfExplorersByMission(listaExplorers))
+console.log(ExplorerService.getExplorersUsernamesByMission(listaExplorers))
