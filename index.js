bae4e7b28d1a8900c631074af4028da5493bd3fa
jricardocam <jricardocam@gmail.com>
2022-04-26 21:10:27 -0500

    Quitando comentarios en archivo Index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Reader = require("./lib/utils/Reader")
 const ExplorerService=require("./lib/services/ExplorerService")
 const FizzbuzzService=require("./lib/services/FizzbuzzService")
-/*
+
 const listaExplorers = Reader.readJsonFile("explorers.json")
 console.log(ExplorerService.filterByMission(listaExplorers,"node"))
 console.log(ExplorerService.getAmountOfExplorersByMission(listaExplorers,"java"))
 console.log(ExplorerService.getExplorersUsernamesByMission(listaExplorers,"java"))
-*/
+
 const explorer1 = {name: "Explorer1", score: 1, trick:0}
 console.log(FizzbuzzService.applyValidationInExplorer(explorer1) )// {name: "Explorer1", score: 1, trick: 1} 
 
